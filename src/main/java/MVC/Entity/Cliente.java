fea7067d90d90b68fb6ee6d34ff20093b80e4d4d
Daniel <dani.pena1244@gmail.com>
2021-10-19 11:53:34 +0200

    Cambios postgre1

--- src/main/java/MVC/Entity/Cliente.java
+++ src/main/java/MVC/Entity/Cliente.java
@@ -3,7 +3,7 @@ package MVC.Entity;
 import javax.persistence.*;
 
 @Entity
-@Table(name="cliente")
+@Table(name="cliente", schema="gestionpedidosCRUD")
 public class Cliente {
     public Cliente() {
     }
