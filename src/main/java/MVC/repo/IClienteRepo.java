0439f9adf79e10675a9f636309ba1c1bce0ec8af
Daniel <dani.pena1244@gmail.com>
2021-11-15 15:08:32 +0100

    Cambios postgre1

--- src/main/java/MVC/repo/IClienteRepo.java
+++ src/main/java/MVC/repo/IClienteRepo.java
@@ -4,6 +4,5 @@ import MVC.Entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface IClienteRepo extends JpaRepository<Cliente, Integer> {
 }
